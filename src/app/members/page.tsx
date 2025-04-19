4a3db8ddddb01ab45971e36ed7c8e75943808479
Jay Mahabal <jmahabal@gmail.com>
2025-04-18 22:49:51 -0400

    add photos and fix users page

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,5 +1,8 @@
 import { getAllCastMembers } from "@/lib/contentful-data";
 import { Link } from "@/components/ui/link";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import { getAssetUrl } from "@/utils/content-helpers";
 
 export default function MembersPage() {
   const castMembers = getAllCastMembers();
@@ -7,22 +10,45 @@ export default function MembersPage() {
   return (
     <div className="mx-auto max-w-7xl px-4 py-24 md:py-16">
       <h1 className="mb-12 text-4xl text-center">Members</h1>
-      <div className="grid gap-10 md:grid-cols-3 lg:grid-cols-4 items-center">
+      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {castMembers
           .sort((a, b) =>
             String(a.fields.name ?? "").localeCompare(
               String(b.fields.name ?? "")
             )
           )
-          .map((member) => (
-            <Link
-              key={member.sys.id}
-              href={`/members/${member.sys.id}`}
-              className="text-center"
-            >
-              {String(member.fields.name ?? "Cast Member")}
-            </Link>
-          ))}
+          .map((member) => {
+            const name = String(member.fields.name ?? "Cast Member");
+            const hometown = member.fields.hometown
+              ? String(member.fields.hometown)
+              : undefined;
+            const photoUrl = getAssetUrl(member.fields.photo);
+
+            return (
+              <Card
+                key={member.sys.id}
+                className="border rounded gap-4 flex flex-row items-center "
+              >
+                {photoUrl && (
+                  <Image
+                    src={`https:${photoUrl}`}
+                    alt={name}
+                    width={48}
+                    height={48}
+                    className="rounded-full object-cover aspect-square"
+                  />
+                )}
+                <div className="flex flex-col items-start gap-0">
+                  <Link href={`/members/${member.sys.id}`} className="w-fit">
+                    <h4 className="text-lg">{name}</h4>
+                  </Link>
+                  {hometown && (
+                    <p className="text-sm text-muted-foreground">{hometown}</p>
+                  )}
+                </div>
+              </Card>
+            );
+          })}
       </div>
     </div>
   );
